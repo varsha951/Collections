94289d4beb1988c38acdb71c28459a09c92930fd
varsha <varsha95155@gmail.com>
2023-02-28 15:38:21 +0530

    apend the elements to last of linkedList

--- uis/List2/LinkedListDoc.java
+++ uis/List2/LinkedListDoc.java
@@ -1,10 +1,22 @@
 package com.uis.List2;
 
+import java.util.LinkedList;
+
 public class LinkedListDoc {
 
 	public static void main(String[] args) {
+		LinkedList list=new LinkedList();
 		
-
+		//1. WAP to append the specified element to the end of a LinkedList.
+         list.add("red");
+         list.add("green");
+         list.add("black");
+         list.add("white");
+         list.add("pink");
+         list.add("yellow");
+         
+         System.out.println(list);
+         
 	}
 
 }
